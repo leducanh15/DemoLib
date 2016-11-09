38169962f1cfaab442281cf8185d994026bc02ca
hcmut15 <leducanh.bkit10@gmail.com>
2016-11-09 20:10:54 +0700

    first commit

---
+++ app/src/main/java/com/eaglecs/newlib/Def.java
@@ -0,0 +1,33 @@
+package com.eaglecs.newlib;
+
+/**
+ * Created by leducanh on 04/10/2016.
+ */
+
+public class Def {
+    //spend
+    public static final int COFFEE = 0;
+    public static final int SHOPPING = 1;
+    public static final int RESTAURANT = 2;
+    public static final int FRIEND = 3;
+
+    // REVENUES
+
+    public static final int SALARY = 4;
+    public static final int BONUS = 5;
+    public static final int GIFT = 6;
+    public static final int PROFIT = 7;
+
+    // TYPE EVENT
+
+    public static final int REVENUE = 1;
+    public static final int SPEND = 2;
+    public static final int DEBT = 3;
+    public static final int ALL = 4;
+
+
+    // Intent
+
+    public static final String STATUS_TRANSACTION = "STATUS_TRANSACTION";
+
+}
